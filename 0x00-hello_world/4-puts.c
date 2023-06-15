96330bf57fe940509a1b5c8c6296fb497760ef7b
Diski007 <Diskvin3007@gmail.com>
2023-06-15 14:16:27 -0700

    Use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ /**
+  * main - Using the puts funtion rather than using printf
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
