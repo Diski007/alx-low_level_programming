94b53861e262e7bdc43fbf8a5fa232d3dc9191fa
Diski007 <Diskvin3007@gmail.com>
2023-06-26 12:05:49 -0700

    Takes a pointer using int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - A pointer to an int as parameter
+ * and the value it points to 98
+ * @n : An integer
+ * Return - n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
