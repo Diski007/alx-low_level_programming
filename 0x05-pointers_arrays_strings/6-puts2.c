dc5ee9995960a4f6a2f793c5afb7071f4d001384
Diski007 <Diskvin3007@gmail.com>
2023-06-26 13:09:02 -0700

    prints every other character of a string, starting with the first character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int lon = 0;
+	int v = 0;
+	char *y = str;
+	int x;
+
+	while (*y != '\0')
+	{
+		y++;
+		lon++;
+	}
+	v = lon - 1;
+	for (x = 0 ; x <= t ; x++)
+	{
+		if (x % 2 == 0)
+	{
+		_putchar(str[x]);
+	}
+	}
+	_putchar('\n');
+}
